1436dd77dc377eb974d96eb2e8c3a3904a4876e5
Akhil Kumar <akhilkumarbnk@gmail.com>
2026-09-14 18:29:21 +0530

    Confirm Android V2 embedding setup

--- android/app/src/main/kotlin/com/example/purix_academy/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/purix_academy/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.purix_academy
+package com.example.purix_academy // (आपका जो पैकेज नेम हो)
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
